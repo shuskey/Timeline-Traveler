66320cf1897513c4a73f427a427ee10e7394bc08
Scott Huskey <scott@photoloom.com>
2025-05-21 13:57:44 -0700

    Update HallOfFamilyPhotos.cs to ensure at least one photo panel is added when a person's life span is zero. This change improves the robustness of the photo display logic.

--- Assets/Scripts/GameObjectScripts/HallOfFamilyPhotos.cs
+++ Assets/Scripts/GameObjectScripts/HallOfFamilyPhotos.cs
@@ -58,7 +58,8 @@ public class HallOfFamilyPhotos : MonoBehaviour
             var allPhotos = _pictureProvider.GetPhotoInfoListForPerson(newfocusPerson.dataBaseOwnerID, birthDate);
             var photoCount = allPhotos.Count;
 
-            for (int age = 0; age < lifeSpan; age++)
+            // If the person has no life span, set the loop to at least add 1 photo panel
+            for (int age = 0; age < Math.Max(1, lifeSpan); age++)
             {
                 int year = birthDate + age;
                 Vector3 position = new Vector3(x - 5.5f, y + 2f, (year) * 5 + 2.5f);
